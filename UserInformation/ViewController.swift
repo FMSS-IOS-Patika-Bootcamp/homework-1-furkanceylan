7a8a7a46e9d59018ca3f1c1687d1ddd73a56c841
Furkan <furkn.ceyln@gmail.com>
2022-09-05 21:57:30 +0300

    Initial Commit

---
+++ UserInformation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserInformation
+//
+//  Created by Furkan Ceylan on 5.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
