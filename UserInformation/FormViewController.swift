f6ec49b467d56c2a9c02a7e58db0b45c4c242c20
Furkan <furkn.ceyln@gmail.com>
2022-09-12 22:30:26 +0300

    .

--- UserInformation/FormViewController.swift
+++ UserInformation/FormViewController.swift
@@ -7,23 +7,40 @@
 
 import UIKit
 
+typealias formHandler = (String?,String?,String?,String?) -> Void
+
 class FormViewController: UIViewController {
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var eMailTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    
+    var formCompliation: formHandler? = nil
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(gestureRecognizer)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        
+        guard let firstName = firstNameTextField.text else {return}
+        guard let lastName = lastNameTextField.text else {return}
+        guard let eMail = eMailTextField.text else {return}
+        guard let phoneNumber = phoneNumberTextField.text else {return}
+        
+        formCompliation?(firstName,lastName,eMail,phoneNumber)
+        navigationController?.popViewController(animated: true)
     }
-    */
-
+    
+    @objc func hideKeyboard(){
+        view.endEditing(true)
+    }
+    
 }
--- UserInformation/ViewController.swift
+++ UserInformation/ViewController.swift
@@ -2,18 +2,45 @@
 //  ViewController.swift
 //  UserInformation
 //
-//  Created by Furkan Ceylan on 5.09.2022.
+//  Created by Furkan Ceylan on 10.09.2022.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstNameLabel: UILabel!
+    @IBOutlet weak var lastNameLabel: UILabel!
+    @IBOutlet weak var eMailLabel: UILabel!
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(toFormVC))
     }
-
-
+    
+    @objc func toFormVC(){
+        performSegue(withIdentifier: "toFormVC", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toFormVC"{
+            if let formVC = segue.destination as? FormViewController{
+                formVC.formCompliation = { firstName, lastName, eMail, phoneNumber in
+                    
+                    guard let firstName = firstName else {return}
+                    guard let lastName = lastName else {return}
+                    guard let eMail = eMail else {return}
+                    guard let phoneNumber = phoneNumber else {return}
+                    
+                    self.firstNameLabel.text = "First Name : \(firstName)"
+                    self.lastNameLabel.text = "Last Name : \(lastName)"
+                    self.eMailLabel.text = "E-mail : \(eMail)"
+                    self.phoneNumberLabel.text = "Phone Number : \(phoneNumber)"
+                }
+            }
+        }
+    }
 }
-
